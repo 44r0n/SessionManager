2a2b9decd5797029758bd99643e5e554d750f116
44r0n <aaron.sn.1988@gmail.com>
2017-08-16 21:58:05 +0200

    Change username for json

--- models/user/user.go
+++ models/user/user.go
@@ -1,10 +1,9 @@
 package models
 
-
 // User represents the structure of our resource
 type User struct {
-        UserName   string `json:"name"`
-        Email string `json:"email"`
-        Password    string    `json:"pass"`
-        ID     string `json:"id"`
-    }
+	UserName string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	ID       string `json:"id"`
+}
